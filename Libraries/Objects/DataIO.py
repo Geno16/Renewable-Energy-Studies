f8c07703f64306521d71f00ab0ea3358d8fb1b6a
Spencer Leifeld <C-Spencer.Leifeld@charter.com>
2022-02-14 10:59:09 -0600

    Changed how data is indexed.
    Signed-off-by: Spencer Leifeld <C-Spencer.Leifeld@charter.com>

--- Libraries/Objects/DataIO.py
+++ Libraries/Objects/DataIO.py
@@ -1,6 +1,8 @@
 # Public Module Imports
+from ast import Return
 import requests
 import json
+import pandas as pd
 
 # Publci Class Imports
 from abc import ABC
@@ -21,9 +23,14 @@ class Abstract_Data_Requestor(ABC):
         self.__textJSON = self.__http.text
         self.__url = retrival_url
 
+    @abstract
+    def Get_DF(self, name='Data'):
+        pass
+
     def Get_HTTP(self):
         return self.__http
 
+    @abstract
     def Get_Dict(self):
         return json.loads(self.__textJSON)
 
@@ -36,9 +43,48 @@ class Abstract_Data_Requestor(ABC):
 
 class Series_Data_Requestor(Abstract_Data_Requestor):
     def __init__(self, series_id):
+        self.__series_id = series_id
         super().__init__(Vars.SERIES_URL + Vars.SERIES_TOKEN.format(series_id))
+
+    def Get_Dict(self):
+        return super().Get_Dict()['series']
+
+    def Get_DF(self, name=None):
+
+        if pd.isnull(name):
+            name = 'Data'
+
+        # Create the Initial Dataframe
+        df = pd.DataFrame(data=self.Get_Dict()[0]['data'], columns=[
+            'Month', name])
+
+        # split the ID, and
+        # add values for indexing
+        idList = self.__series_id.split(".")
+        df['Category'] = idList[0]
+        df['Sub-Category'] = idList[1]
+        df['State'] = idList[2].split('-')[1]
+
+        # Return the re-indexed dataframe as a series.
+        return df.set_index(keys=['Category', 'Sub-Category', 'State', 'Month']).squeeze()
 
 
 class Catagory_Data_Requestor(Abstract_Data_Requestor):
     def __init__(self, catagory_id):
         super().__init__(Vars.CATAGORY_URL + Vars.CATAGORY_TOKEN.format(catagory_id))
+
+    def Get_Dict(self):
+        return super().Get_Dict()['category']
+
+    def Get_DF(self, name=None):
+        dict = self.Get_Dict()
+        dfList = []
+        for series in dict['childseries']:
+            if pd.isnull(name):
+                name = ' - '.join(series['name'].split(' : ')[0:2])
+
+            if ".M" in series['series_id']:
+                dfList.append(Series_Data_Requestor(
+                    series['series_id']).Get_DF(name=name))
+
+        return pd.concat(dfList)
