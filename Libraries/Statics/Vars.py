443ea9046444b8eb554b00ac122bf3dd895df318
Spencer Leifeld <C-Spencer.Leifeld@charter.com>
2022-02-14 09:17:05 -0600

    Fixed Misspelling.
    Signed-off-by: Spencer Leifeld <C-Spencer.Leifeld@charter.com>

--- Libraries/Statics/Vars.py
+++ Libraries/Statics/Vars.py
@@ -5,7 +5,7 @@ API_KEY = sys.argv[1]
 SERIES_TOKEN = "&series_id={}"
 CATAGORY_TOKEN = "&category_id={}"
 SERIES_INDICATOR = "series"
-CATAGORY_INDICATOR = "catagory"
+CATAGORY_INDICATOR = "category"
 URL = 'https://api.eia.gov/{}/?api_key={}'
 SERIES_URL = URL.format(SERIES_INDICATOR, sys.argv[1])
 CATAGORY_URL = URL.format(CATAGORY_INDICATOR, sys.argv[1])
